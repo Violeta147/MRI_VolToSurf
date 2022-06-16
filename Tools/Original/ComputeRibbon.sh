f6088aff424a9c6623dce098bbaf9c6d5df7d684
Violeta Sanchez <viole1998@gmail.com>
2022-06-16 10:43:50 +0200

    Archivo original

--- Tools/Original/ComputeRibbon.sh
+++ Tools/Original/ComputeRibbon.sh
@@ -14,22 +14,18 @@ RefImg=$3
 hemi=$4
 GreyRibbonValue=$5
 
-# DataFolder=/media/BabyBrain/preterm
-# OutFolder=/media/BabyBrain/preterm/fMRI_Vol2Cifti
-
 # Compute shortest distance from each voxel to surfaces
-wb_command -create-signed-distance-volume "${DataFolder}"/dhcp_anat_pipeline/"${Subject}"/"${Ses}"/anat/"${Subject}"_"${Ses}"_hemi-"${hemi}"_space-T2w_wm.surf.gii "${RefImg}" "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white.native.nii.gz
-wb_command -create-signed-distance-volume "${DataFolder}"/dhcp_anat_pipeline/"${Subject}"/"${Ses}"/anat/"${Subject}"_"${Ses}"_hemi-"${hemi}"_space-T2w_pial.surf.gii "${RefImg}" "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial.native.nii.gz
+wb_command -create-signed-distance-volume "${DataFolder}"/hemi-"${hemi}"_white.surf.gii "${RefImg}" "${OutFolder}"/"${hemi}".white.native.nii.gz
+wb_command -create-signed-distance-volume "${DataFolder}"/hemi-"${hemi}"_pial.surf.gii "${RefImg}" "${OutFolder}"/"${hemi}".pial.native.nii.gz
  
 # Get voxels out of WM surface
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white.native.nii.gz -thr 0 -bin -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz -bin "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz
+fslmaths "${OutFolder}"/"${hemi}".white.native.nii.gz -thr 0 -bin -mul 255 "${OutFolder}"/"${hemi}".white_thr0.native.nii.gz
+fslmaths "${OutFolder}"/"${hemi}".white_thr0.native.nii.gz -bin "${OutFolder}"/"${hemi}".white_thr0.native.nii.gz
 
 # Get voxels into Pial surface
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial.native.nii.gz -uthr 0 -abs -bin -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz -bin "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz
+fslmaths "${OutFolder}"/"${hemi}".pial.native.nii.gz -uthr 0 -abs -bin -mul 255 "${OutFolder}"/"${hemi}".pial_uthr0.native.nii.gz
+fslmaths "${OutFolder}"/"${hemi}".pial_uthr0.native.nii.gz -bin "${OutFolder}"/"${hemi}".pial_uthr0.native.nii.gz
 
 # Get intersecting voxels between two masks
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz -mas "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz
-
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz -bin -mul "${GreyRibbonValue}" "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz
+fslmaths "${OutFolder}"/"${hemi}".pial_uthr0.native.nii.gz -mas "${OutFolder}"/"${hemi}".white_thr0.native.nii.gz -mul 255 "${OutFolder}"/"${hemi}".ribbon.nii.gz
+fslmaths "${OutFolder}"/"${hemi}".ribbon.nii.gz -bin -mul "${GreyRibbonValue}" "${OutFolder}"/"${hemi}".ribbon.nii.gz
