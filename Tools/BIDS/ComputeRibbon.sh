ce2bc33e715e4ba5af66f92277daa6bf69385511
Violeta Sanchez <viole1998@gmail.com>
2022-06-17 11:15:56 +0200

    Corrección de variables y directorios (funciona)

--- Tools/BIDS/ComputeRibbon.sh
+++ Tools/BIDS/ComputeRibbon.sh
@@ -8,28 +8,36 @@
 ###	- hemi: The hemisphere to be computed as spelled in the surface files
 ###	- GreyRibbonValue: The value of the grey ribbon voxels in the output image
 
-DataFolder=$1
-OutFolder=$2
-RefImg=$3
-hemi=$4
-GreyRibbonValue=$5
 
-# DataFolder=/media/BabyBrain/preterm
-# OutFolder=/media/BabyBrain/preterm/fMRI_Vol2Cifti
+AnatFolder=/media/BabyBrain/preterm/dhcp_anat_pipeline/"${Subject}"/"${Ses}"/anat
+OutFolder=/media/BabyBrain/preterm/fMRI_Vol2Cifti/"${Subject}"_"${Ses}"/tmp
+AnatFolder=$1
+OutFolder=$2
+Subject=$3
+Ses=$4
+RefImg=$5
+hemi=$6
+GreyRibbonValue=$7
+
+# Check data:
+echo "AnatFolder:" "${AnatFolder}"
+echo "OutFolder:" "${OutFolder}"
+echo "RefImg:" "${RefImg}"
+echo "GreyRibbonValue:" "${GreyRibbonValue}"
 
 # Compute shortest distance from each voxel to surfaces
-wb_command -create-signed-distance-volume "${DataFolder}"/dhcp_anat_pipeline/"${Subject}"/"${Ses}"/anat/"${Subject}"_"${Ses}"_hemi-"${hemi}"_space-T2w_wm.surf.gii "${RefImg}" "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white.native.nii.gz
-wb_command -create-signed-distance-volume "${DataFolder}"/dhcp_anat_pipeline/"${Subject}"/"${Ses}"/anat/"${Subject}"_"${Ses}"_hemi-"${hemi}"_space-T2w_pial.surf.gii "${RefImg}" "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial.native.nii.gz
+wb_command -create-signed-distance-volume "${AnatFolder}"/"${Subject}"_"${Ses}"_hemi-"${hemi}"_space-T2w_wm.surf.gii "${RefImg}" "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".white.native.nii.gz
+wb_command -create-signed-distance-volume "${AnatFolder}"/"${Subject}"_"${Ses}"_hemi-"${hemi}"_space-T2w_pial.surf.gii "${RefImg}" "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".pial.native.nii.gz
  
 # Get voxels out of WM surface
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white.native.nii.gz -thr 0 -bin -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz -bin "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz
+fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".white.native.nii.gz -thr 0 -bin -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz
+fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz -bin "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz
 
 # Get voxels into Pial surface
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial.native.nii.gz -uthr 0 -abs -bin -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz -bin "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz
+fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".pial.native.nii.gz -uthr 0 -abs -bin -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz
+fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz -bin "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz
 
 # Get intersecting voxels between two masks
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz -mas "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz
+fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".pial_uthr0.native.nii.gz -mas "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".white_thr0.native.nii.gz -mul 255 "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz
 
-fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz -bin -mul "${GreyRibbonValue}" "${OutFolder}"/"${Subject}"_"${Ses}"/tmp/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz
+fslmaths "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz -bin -mul "${GreyRibbonValue}" "${OutFolder}"/"${Subject}"_"${Ses}"_"${hemi}".ribbon.nii.gz
